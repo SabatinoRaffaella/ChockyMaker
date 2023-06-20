e421115df5f067ae3665933a6edc8ad29615166c
RFirewalker <131981162+RFirewalker@users.noreply.github.com>
2023-06-20 16:36:23 +0200

    email
    Added some local mailbox, wanna check out what Sonarcloud thinks of it

---
+++ src/java/Controller/Email/SendEmail.java
@@ -0,0 +1,49 @@
+package Controller.Email;
+import Model.User;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name = "SendEmail", urlPatterns = {"/SendEmail"})
+public class SendEmail extends HttpServlet {
+  @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        // reads form fields
+        User usr = (User)request.getSession().getAttribute("user");
+        String onepass = usr.getPassword();
+        String recipient = usr.getEmail();
+        String subject = "Complete Registration:";
+        String content = "We are sending to you the one-time password to complete registration.\n"+onepass;
+       
+        String resultMessage = "";
+        try {
+            Email em = new Email(recipient,subject,content);
+            EmailList l = new EmailList();
+            l.addEmail(em);
+            request.getSession().setAttribute("emails", l);
+            //request.getSession().setMaxInactiveInterval(0);
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Result.jsp");
+            dispatcher.forward(request,response);   
+            resultMessage = "The e-mail was sent successfully";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            resultMessage = "There were an error: " + ex.getMessage();
+        } finally {
+            request.setAttribute("Message", resultMessage);
+            getServletContext().getRequestDispatcher("/Result.jsp").forward(
+                    request, response);
+        }
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
